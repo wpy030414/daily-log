f7734a9251489051ec4ed42429339be61dd3a3e8
杏仁鹿 <penyoofficial@outlook.com>
2025-04-09 22:02:52 +0800

    feat: construct trusted context during preview

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    basicSsl({
+      name: 'Penyo',
+      domains: ['oss.ahkdxx.cn', 'nas.localhost'],
+      certDir: 'dist/cert',
+    }),
     vueDevTools(),
   ],
   resolve: {
